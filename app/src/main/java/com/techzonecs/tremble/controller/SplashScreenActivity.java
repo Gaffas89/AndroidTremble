18c51be9e2fb93c0278a1cc1d28e48ccf1f73ceb
ShBouyabes <Sh.Bouyabes@gmail.com>
2015-11-01 13:04:14 +0300

    added splash screen

--- app/src/main/java/com/techzonecs/tremble/controller/SplashScreenActivity.java
+++ app/src/main/java/com/techzonecs/tremble/controller/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package com.techzonecs.tremble.controller;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,40 @@ import com.techzonecs.tremble.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    public final static String PREF_NAME="userInfo";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread t=new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+//                    Thread.sleep(3500);
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                SharedPreferences prefs = getSharedPreferences(PREF_NAME, MODE_APPEND);
+                boolean loggedin = prefs.getBoolean("isLoggedIn", false);
+
+                Class nextClass = LoginPageActivity.class;
+
+                if(loggedin){
+                    nextClass= SessionListViewActivity.class;
+                }
+
+                Intent i=new Intent(SplashScreenActivity.this,nextClass);
+                startActivity(i);
+                finish();
+            }
+        });
+        t.start();
+
     }
 }
