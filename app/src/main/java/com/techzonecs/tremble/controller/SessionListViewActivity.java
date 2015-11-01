9a5453d493677c78499a96bc7ba31bffdc0b2712
Gaffas <gaffas.89@gmail.com>
2015-11-01 12:50:14 +0300

    MADE STUFF WORK @_@

--- app/src/main/java/com/techzonecs/tremble/controller/SessionListViewActivity.java
+++ app/src/main/java/com/techzonecs/tremble/controller/SessionListViewActivity.java
@@ -8,9 +8,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.techzonecs.tremble.R;
 import com.techzonecs.tremble.model.Session;
-import com.techzonecs.tremble.utilities.SessionConnection;
+import com.techzonecs.tremble.utilities.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -22,17 +29,73 @@ public class SessionListViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_list_view);
 
-        populateSessionList();
+        getSessionArray("2");
     }
 
+
+
+    public void getSessionArray(String trainee){
+        String tag_string_req = "string_req";
+
+        String url = "http://192.168.1.129:8080/TrembleBackend/GetSessions?id_trainee=" + trainee;
+
+        StringRequest strReq = new StringRequest(Request.Method.GET,
+                url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+
+                arrayOfSessions = parseJson(response);
+                populateSessionList();
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Log.d("error","test1");
+            }
+        });
+
+// Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+
+    }
+
+    private ArrayList<Session> parseJson(String str){
+
+        ArrayList<Session> sessionArrayList = new ArrayList<>();
+        try{
+            JSONObject jsonResponse = new JSONObject(str);
+            JSONArray jsonArray = new JSONArray(jsonResponse.getString("result_data"));
+
+            for(int i = 0; i < jsonArray.length() ; i++)
+            {
+
+                JSONObject json = jsonArray.getJSONObject(i);
+                Session sess = new Session();
+                sess.setClassName(json.getString("class_name"));
+                sess.setCourseName(json.getString("course_name"));
+                sess.setDate(json.getString("wave_date"));
+                sess.setLocation(json.getString("location_name"));
+                sess.setLocationGps(json.getString("location_gps"));
+                sess.setTrainerName(json.getString("trainer_name"));
+                sess.setZone(json.getString("zone"));
+
+                sessionArrayList.add(sess);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return sessionArrayList;
+    }
+
+
     private void populateSessionList() {
-        // Construct the data source
-
-        SessionConnection sessionConnect = new SessionConnection();
-
-        arrayOfSessions = sessionConnect.getSessionArray("2");
-
-        Log.d("testing", arrayOfSessions.toString());
 
         // Create the adapter to convert the array to views
         CustomSessionAdaptor adapter = new CustomSessionAdaptor(this, arrayOfSessions);
